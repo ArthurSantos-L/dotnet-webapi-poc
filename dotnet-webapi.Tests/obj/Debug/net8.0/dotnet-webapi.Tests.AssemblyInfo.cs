05ed8f5aef2bb0f5c966ab3685ff9af51ee94b9c
ArthurSantos-L <arthur.santos@gentrop.com>
2026-01-13 21:11:36 -0300

    test: are now running as expected.

--- dotnet-webapi.Tests/obj/Debug/net8.0/dotnet-webapi.Tests.AssemblyInfo.cs
+++ dotnet-webapi.Tests/obj/Debug/net8.0/dotnet-webapi.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-webapi.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e17c35b2488f68412614a1d97f59fd7e6658e7d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99dd20c9718c05b5fa5a878c78f5c6de91eb34b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-webapi.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-webapi.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
